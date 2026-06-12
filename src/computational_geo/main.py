19de6de57d5998e7143add32750a98ada081f9eb
mban7 <banasikm07@gmail.com>
2026-06-12 15:19:52 +0200

    feat: added commands to run cli or web app

--- src/computational_geo/main.py
+++ src/computational_geo/main.py
@@ -1,2 +1,13 @@
-def main() -> None:
-    print("Hello from computational-geo!")
+import sys
+from pathlib import Path
+
+
+def run_web() -> None:
+    """
+    Uruchamiamy aplikacje Streamlit (web.py)
+    """
+    from streamlit.web import cli as stcli
+
+    web_path: Path = Path(__file__).parent / "web.py"
+    sys.argv = ["streamlit", "run", str(web_path)]
+    sys.exit(stcli.main())
